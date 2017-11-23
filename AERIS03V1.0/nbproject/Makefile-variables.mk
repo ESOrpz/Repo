6b12a9cad50bdcbb91fc56b138c1a77011c4c7b1
Killian MELLE <killian.melle@estaca.eu>
2017-11-23 21:03:46 +0100

    Ajout board 3

---
+++ AERIS03V1.0/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=AERIS03V1.0.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/AERIS03V1.0.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=aeris03v1.0.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/aeris03v1.0.tar
